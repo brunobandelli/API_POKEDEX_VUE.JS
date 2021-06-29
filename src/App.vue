19c2d41284a2ed923bec24643b7591bf2377d9ab
brunobandelli <brunobandelli@gmail.com>
2021-06-28 22:06:33 -0300

    FAZENDO REQUISIÇÃO COM AXIOS NA API

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,23 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios';
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data(){
+    return{
+      pokemons: []
+    }
+  },
+  created: function(){
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=151&offset=0").then(res=> {
+      console.log("Pegou a lista de Pokémons");
+      this.pokemons = res.data.results;
+    })
   }
 }
 </script>
